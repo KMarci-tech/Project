f72ee23fb117e922599f534ce90c25820afb7968
KMarci-tech <71447266+KMarci-tech@users.noreply.github.com>
2021-10-10 20:45:29 +0200

    Add files via upload

---
+++ src/frame/Handler.java
@@ -0,0 +1,43 @@
+package frame;
+
+import java.awt.Graphics;
+import java.util.LinkedList;
+
+public class Handler {
+	
+	public LinkedList<GameObject> object = new LinkedList<GameObject>();
+	
+	
+	private GameObject tempObject;
+	
+	public void update() {
+		
+		for(int i = 0; i < object.size(); i++) {
+			tempObject = object.get(i);
+			
+			tempObject.update(object);
+		}
+	}
+	
+	
+	public void render(Graphics g) {
+		
+		for(int i = 0; i < object.size(); i++) {
+			tempObject = object.get(i);
+			
+			tempObject.render(g);
+		}
+	}
+	
+	public void addObject(GameObject object) {
+		this.object.add(object);
+		
+		
+	}
+	
+	public void removeObject(GameObject object) {
+		this.object.remove(object);
+	}
+	
+	
+}
