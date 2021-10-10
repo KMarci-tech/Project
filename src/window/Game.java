901600acd93036d58ff2f8032b5de72fe1be8cbd
KMarci-tech <71447266+KMarci-tech@users.noreply.github.com>
2021-10-10 20:44:18 +0200

    Add files via upload

---
+++ src/window/Game.java
@@ -0,0 +1,126 @@
+package window;
+
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
+
+import frame.Handler;
+import frame.ObjectId;
+import objects.Test;
+
+public class Game extends Canvas implements Runnable{
+
+	private boolean running = false;
+	private final double updateRate = 1.0d/60.0d;
+	private Thread thread;
+	
+	
+	private long nextStatTime;
+	private int ups = 0; // update per seconds
+	private int fps = 0; // frame per seconds
+	
+	
+	//Object
+	
+	Handler handler;
+	
+	private void init() {
+		
+		handler = new Handler();
+		
+		handler.addObject(new Test(200,200,ObjectId.Test));
+		
+	}
+	
+	
+	public synchronized void start() { // start thread
+		
+		if(running) {
+			return;
+		}
+		
+		running = true;
+		thread = new Thread(this);
+		thread.start();
+	}
+	
+	
+	
+	
+	@Override
+	public void run() { // game loop
+		
+		init();
+		this.requestFocus();
+		double accumulator = 0;
+		long currentTime = System.currentTimeMillis();
+		long lastUpdate = System.currentTimeMillis();
+		nextStatTime = System.currentTimeMillis() + 1000;
+		
+		while(running) {
+			currentTime = System.currentTimeMillis();
+			double lastRenderTimeInSeconds = (currentTime - lastUpdate) / 1000d;
+			accumulator += lastRenderTimeInSeconds;
+			lastUpdate = currentTime;
+			
+			
+			while(accumulator > updateRate) {
+				update();
+				accumulator -= updateRate;
+			}
+			render();
+			printStats();
+		}
+		
+	}
+	
+	private void printStats() {
+		if(System.currentTimeMillis() > nextStatTime) {
+			System.out.println("FPS: " + fps + " UPS: " + ups);
+			fps = 0;
+			ups = 0;
+			nextStatTime = System.currentTimeMillis() + 1000;
+		}
+	}
+	
+	
+	private void render() { 
+		fps++;
+		BufferStrategy bs = this.getBufferStrategy();
+		if(bs == null) {
+			this.createBufferStrategy(3);
+			return;
+		}
+		Graphics g = bs.getDrawGraphics();
+		//////////////////////////////////
+		//Draw Here
+		
+		g.setColor(Color.black);
+		g.fillRect(0,0,getWidth(),getHeight());
+		
+		handler.render(g);
+		
+		
+		///////////////////////////////////////
+		g.dispose();
+		bs.show();
+	}
+	
+	private void update() {
+		ups++;
+		handler.update();
+	}
+	
+	
+	
+	
+	
+	
+	
+	public static void main(String[] args) {
+		
+		new Window(800,600,"Platformer",new Game());
+		
+	}
+}
